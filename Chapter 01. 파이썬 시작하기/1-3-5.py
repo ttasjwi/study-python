b740ee94399e2b6e29c58d695a0207e6a277fef2
ttasjwi <ttasjwi920@gmail.com>
2022-07-14 23:00:29 +0900

    파이썬의 기본 용어 - 출력(print)

---
+++ Chapter 01. 파이썬 시작하기/1-3-5.py
@@ -0,0 +1,15 @@
+# 개행 없이 출력 (java : print)
+print('hello ', end='')
+print('world')
+
+# 개행
+print()
+
+# 개행 출력(java : println)
+print('hello')
+
+# 여러개 출력 (구분자 ' ')
+print('hello', 'world')
+
+# 여러개 출력 (구분자 지정)
+print('a', 'b', 'c', sep=';')
