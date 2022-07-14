c9952adb1d97754bc404ab3dd20b83c544c1a048
ttasjwi <ttasjwi920@gmail.com>
2022-07-14 22:35:08 +0900

    파이썬의 기본 용어 - 캐멀케이스, 스네이크 케이스
    - 캐멀 케이스 : 클래스에 사용 (대문자로 시작)
    - 스네이크 케이스 : 함수, 변수에 사용
      - 뒤에 괄호 있다 : 함수
      - 뒤에 괄호 없다 : 변수

---
+++ Chapter 01. 파이썬 시작하기/1-3-2.py
@@ -0,0 +1,14 @@
+# 식별자 - 캐멀케이스 (대문자로 시작 -> 클래스)
+class HelloMachine:
+    def foo(self):
+        print('hello')
+
+# 식별자 - 스네이크 케이스 (소문자로 시작)
+machine = HelloMachine()
+machine.foo()
+
+# 함수 : 뒤에 괄호가 있다
+print("nyaa")
+
+# 변수 : 뒤에 괄호가 없다
+number_of_people = 5
