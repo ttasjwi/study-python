663e5b447805872c4659c5d8c753c76c03dc3686
ttasjwi <ttasjwi920@gmail.com>
2022-09-05 21:39:44 +0900

    숫자의 종류 : int, float

---
+++ Chapter 02. 자료형/2.2 숫자/2-2-1.py
@@ -0,0 +1,22 @@
+
+# int : 소숫점이 없는 숫자 (정수형)
+print(273)
+print(type(273)) # int
+
+# float : 소숫점이 있는 숫자 (실수형 : 부동소숫점)
+print(52.273)
+print(type(52.273)) # float
+
+print(0)
+print(type(0))
+
+print(0.0)
+print(type(0.0))
+
+
+# 지수표현 가능
+print(0.52273e2) # 52.273
+print(type(0.52273e2))
+
+print(0.52273e-2) # 0.0052273
+print(type(0.52273e-2))
