fe94ed649f3f2ad830157d68472c059d3579e70e
ttasjwi <ttasjwi920@gmail.com>
2022-09-05 21:46:36 +0900

    int(...) : 문자열을 정수로 타입캐스팅

---
+++ Chapter 02. 자료형/2.3 변수와 입력/2-3-6.py
@@ -0,0 +1,10 @@
+
+# 문자열을 숫자로 바꾸기
+string_a = input('입력 273>')
+int_a = int(string_a)
+
+string_b = input('입력 52>')
+int_b = int(string_b)
+
+print('문자열 자료 :',string_a + string_b)
+print('숫자 자료 :', int_a + int_b)
