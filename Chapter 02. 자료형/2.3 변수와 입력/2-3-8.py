bdf891ff685324fde9371c548b33d6dbab6aa2a0
ttasjwi <ttasjwi920@gmail.com>
2022-09-05 21:47:49 +0900

    input과 float 함수를 함께 조합해서 사용

---
+++ Chapter 02. 자료형/2.3 변수와 입력/2-3-8.py
@@ -0,0 +1,10 @@
+
+# input과 float() 함수 조합
+
+input_a = float(input('첫번째 숫자>')) # 273 입력
+input_b = float(input('두번째 숫자>')) # 52 입력
+
+print('덧셈 결과 :', input_a+input_b)
+print('뺄셈 결과 :', input_a-input_b)
+print('곱셈 결과 :', input_a*input_b)
+print('나눗셈 결과 :', input_a/input_b)
