cf96136e200d0be86112453f856d82c7f347d2cd
ttasjwi <ttasjwi920@gmail.com>
2022-09-05 21:43:19 +0900

    복합 대입 연산자

---
+++ Chapter 02. 자료형/2.3 변수와 입력/2-3-3.py
@@ -0,0 +1,18 @@
+# 복합 대입 연산자
+# += : 덧셈 후 대입
+# -= : 뺄셈 후 대입
+# *= : 곱셈 후 대입
+# /= : 나눗셈 후 대입
+# %= : 나머지를 구한 후 대입
+# **= : 제곱 후 대입
+number = 100
+print('number =', number)
+
+number += 10
+print('number =', number)
+
+number += 20
+print('number =', number)
+
+number += 30
+print('number =', number)
