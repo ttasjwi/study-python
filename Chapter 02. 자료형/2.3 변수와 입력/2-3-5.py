c6172921b66b38f529dedf3979635f24f42ff1a1
ttasjwi <ttasjwi920@gmail.com>
2022-09-05 21:46:08 +0900

    input : 입력을 무조건 string 타입으로 받음

---
+++ Chapter 02. 자료형/2.3 변수와 입력/2-3-5.py
@@ -0,0 +1,11 @@
+
+# 사용자 입력 : input()
+# input 함수를 통해 입력하면 무조건 문자열 자료형을 리턴한다
+
+string = input('인사말을 입력하세요> ')
+print(string)
+print(type(string)) # 기본적으로는 문자열로 입력받는다
+
+number = input('숫자를 입력하세요> ')
+print(number)
+print(type(number))
