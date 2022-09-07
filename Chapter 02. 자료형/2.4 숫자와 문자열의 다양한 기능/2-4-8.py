a16c9f3f41c675792fc6d0db088081a6148a1fd0
ttasjwi <ttasjwi920@gmail.com>
2022-09-07 19:19:09 +0900

    부동 소숫점 출력의 다양한 형태

---
+++ Chapter 02. 자료형/2.4 숫자와 문자열의 다양한 기능/2-4-8.py
@@ -0,0 +1,8 @@
+
+output_a = "{:15.3f}".format(52.273) # 소숫점 아래 자릿수 지정
+output_b = "{:15.2f}".format(52.273)
+output_c = "{:15.1f}".format(52.273)
+
+print(output_a)
+print(output_b)
+print(output_c)
