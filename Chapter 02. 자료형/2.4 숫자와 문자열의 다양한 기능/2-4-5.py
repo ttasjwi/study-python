3f9332bcff4a63fe726d8d5fdf2ac2500b0f663d
ttasjwi <ttasjwi920@gmail.com>
2022-09-07 19:09:35 +0900

    정수 출력의 다양한 형태

---
+++ Chapter 02. 자료형/2.4 숫자와 문자열의 다양한 기능/2-4-5.py
@@ -0,0 +1,11 @@
+# 부호와 함께 출력하기
+output_f = "{:+d}".format(52) # 양수 부호랑
+output_g = "{:+d}".format(-52) # 음수 부호랑
+output_h = "{: d}".format(52) # 양수 : 부호 부분 공백
+output_i = "{: d}".format(52) # 음수 : 부호 부분이 양수일 때 공백
+
+print("# 부호와 함께 출력하기")
+print(output_f)
+print(output_g)
+print(output_h)
+print(output_i)
