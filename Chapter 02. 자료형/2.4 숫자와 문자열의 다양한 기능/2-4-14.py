8dca139147ffcea48996c778dc74ec74571c9bc9
ttasjwi <ttasjwi920@gmail.com>
2022-09-07 19:56:38 +0900

    문자열 in 문자열 : 앞 문자열이 뒷 문자열에 포함되어 있는지 여부를 반환

---
+++ Chapter 02. 자료형/2.4 숫자와 문자열의 다양한 기능/2-4-14.py
@@ -0,0 +1,10 @@
+# 문자열A in 문자열B : 문자열A가 문자열B에 포함되어 있는 지 여부를 반환
+
+str = "안녕"
+print(f"str = {str}")
+print("str in \"안녕하세요\" = {}".format(str in "안녕하세요"))
+print()
+
+str = "땃쥐"
+print(f"str = {str}")
+print("str in \"안녕하세요\" = {}".format(str in "안녕하세요"))
