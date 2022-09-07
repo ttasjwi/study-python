b00de00a785c29bc1a96984f857145c969fda033
ttasjwi <ttasjwi920@gmail.com>
2022-09-07 20:45:47 +0900

    and, or, not

---
+++ Chapter 03. 불 자료형과 if 조건문/3.1 불 자료형과 if 조건문/3-1-2.py
@@ -0,0 +1,16 @@
+# 논리 연산자 : not, and, or
+
+# not
+print(not True)
+print(not False)
+
+x = 10
+print("x < 20 = ", x < 20)
+print("not (x < 10) = ", not (x < 10))
+
+# and : 둘다 true일 때 true
+# or : 둘 중 하나가 true 일때 true
+x = 20
+print('x = ', x)
+print('10 < x and x < 30 = ', 10 < x and x > 30)
+print('10 < x or x < 30 = ', 10 < x or x < 30)
