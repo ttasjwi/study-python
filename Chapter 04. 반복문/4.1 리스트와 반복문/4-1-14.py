897fd9bcf688470a5d4fbd01b9aa53f3e6d0df87
ttasjwi <ttasjwi920@gmail.com>
2022-09-10 22:47:25 +0900

    for i in range(start, end) 및 for i in range(n)
    - 시작~끝(미포함)
    - n회 반복, (0~n-1)

---
+++ Chapter 04. 반복문/4.1 리스트와 반복문/4-1-14.py
@@ -0,0 +1,7 @@
+# for i in range(start(포함), end(미포함)) : 일반적인 for문
+for i in range(0, 10):
+    print(i, '나는 빡빡이다')
+
+# for i in range(n) : start에 0부터 n-1까지 (총 n회 반복)
+for i in range(10):
+    print(i, '반복')
