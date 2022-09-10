e5bc465048087f130c224f076e1a13d4e95ccc15
ttasjwi <ttasjwi920@gmail.com>
2022-09-10 22:38:53 +0900

    for 요소 in 반복대상
    - java의 enhanced For문과 비슷하다.
    - 리스트, 문자열, ... 반복할 수 있는 모든 것에 대해서 가능하다

---
+++ Chapter 04. 반복문/4.1 리스트와 반복문/4-1-13.py
@@ -0,0 +1,12 @@
+# for 반복분 (with : 리스트)
+array = [273, 32, 103, 57, 52]
+for element in array:
+    print(element)
+
+print()
+
+# for 반복문 (with : 문자열)
+string = '안녕하세요'
+for character in string:
+    print(character)
+
