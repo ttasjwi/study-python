0e81d3c3f0ba6ebe08d154832d00257d2e8ee7bb
ttasjwi <ttasjwi920@gmail.com>
2022-09-10 21:30:13 +0900

    리스트 선언, 각 요소 접근(인덱싱)

---
+++ Chapter 04. 반복문/4.1 리스트와 반복문/4-1-1.py
@@ -0,0 +1,26 @@
+# 리스트
+
+array = [273, 32, 108, '문자열', True, False]
+print(array)
+print()
+
+# 리스트 각 요소 접근(인덱싱)
+print(array[0])
+print(array[1])
+print(array[2])
+print(array[3])
+print(array[4])
+print(array[5])
+print()
+
+# 뒤에서부터 인덱싱
+print(array[-1])
+print(array[-2])
+print(array[-3])
+print(array[-4])
+print(array[-5])
+print(array[-6])
+print()
+
+# 범위 선택
+print(array[2:4])
