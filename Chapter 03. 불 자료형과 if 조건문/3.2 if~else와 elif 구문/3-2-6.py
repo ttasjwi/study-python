8b9c4c873a0b320219f61afc4584e6d5280fef17
ttasjwi <ttasjwi920@gmail.com>
2022-09-10 21:17:27 +0900

    pass 대신 raise NotImplementError를 통해 예외를 발생시킬 수 있음.

---
+++ Chapter 03. 불 자료형과 if 조건문/3.2 if~else와 elif 구문/3-2-6.py
@@ -0,0 +1,10 @@
+# 입력을 받습니다
+number = int(input('정수 입력> '))
+
+# 조건문 사용
+if number > 0:
+    # 양수일 때 : 아직 미구현 상태입니다.
+    raise NotImplementedError
+else:
+    # 음수일 대 : 아직 미구현 상태입니다.
+    raise NotImplementedError
