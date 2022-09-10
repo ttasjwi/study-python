3d9def057fd84466d678de99098f3a31ed340dc8
ttasjwi <ttasjwi920@gmail.com>
2022-09-10 22:25:30 +0900

    리스트 정렬 : list.sort()
    - sort() : 기본 정렬 (오름차순)
    - sort(reverse=True) : 내림차순 정렬

---
+++ Chapter 04. 반복문/4.1 리스트와 반복문/4-1-11.py
@@ -0,0 +1,12 @@
+list_e = [52, 273, 102, 32, 275, 1, 7]
+print('list_e = ', list_e)
+print()
+
+list_e.sort()
+print('list_e.sort()') # 기본 : 오름차순 정렬
+print('list_e = ', list_e)
+print()
+
+list_e.sort(reverse=True) # 내림차순 정렬
+print('list_e.sort(reverse=True)')
+print('list_e = ', list_e)
