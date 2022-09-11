b20725eac9bca97c06782a004ecdcfd7d47fd693
ttasjwi <ttasjwi920@gmail.com>
2022-09-11 12:00:04 +0900

    for 반복문과 범위 자료형 혼합(일반적인 for문)

---
+++ Chapter 04. 반복문/4.3 범위 자료형과 while 반복문/4-3-2.py
@@ -0,0 +1,13 @@
+# for문 : 범위와 사용
+
+for i in range(5):
+    print(f'{i} = 반복 변수')
+print()
+
+for i in range(5, 10):
+    print(f'{i} = 반복 변수')
+print()
+
+for i in range(0, 10, 3):
+    print(f'{i} = 반복 변수')
+print()
