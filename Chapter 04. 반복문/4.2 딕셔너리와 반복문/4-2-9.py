9091a7032e2b185ca1f7fd60b39dc5a6c3aeb550
ttasjwi <ttasjwi920@gmail.com>
2022-09-11 11:44:22 +0900

    딕셔너리 반복문 : for 키 in 딕셔너리

---
+++ Chapter 04. 반복문/4.2 딕셔너리와 반복문/4-2-9.py
@@ -0,0 +1,15 @@
+# 딕셔너리 반복문
+# for 키 in  딕셔너리
+
+
+# 딕셔너리 선언
+dictionary = {
+    'name': '7D 건조 망고',
+    'type': '당절임',
+    'ingredient': ['망고', '설탕', '메타중아황산나트륨', '치자황색소'],
+    'origin': '필리핀'
+}
+
+# for 문
+for key in dictionary:
+    print('key:', dictionary[key])
