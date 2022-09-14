6a23f1416348a748ee48fe0534ad7241c89d0e9b
ttasjwi <ttasjwi920@gmail.com>
2022-09-14 14:29:14 +0900

    리스트 함수 - min, max, sum
    - min(리스트) : 리스트 내부 최솟값
    - max(리스트) : 리스트 내부 최댓값
    - sum(리스트) : 리스트 내부 값을 모두 합산

---
+++ Chapter 04. 반복문/4.4 문자열, 리스트, 딕셔너리와 관련된 기본 함수/4-4-1.py
@@ -0,0 +1,13 @@
+# min(리스트) : 리스트 내부 최솟값
+# max(리스트) : 리스트 내부 최댓값
+# sum(리스트) : 리스트 내부 값을 모두 합산
+
+numbers = [103, 52, 273, 32, 77]
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+# min(인자1, 인자2, ...) : 인자들 중 최솟값
+# max(인자1, 인자2, ...) : 인자들 중 최댓값
+print(min(1, 2))
+print(max(1, 2))
