8991e3d802dcb8617e427115e90ef3b7b3ab729f
ttasjwi <ttasjwi920@gmail.com>
2022-09-21 21:47:59 +0900

    파이썬에서 클래스를 선언하고, 인스턴스를 생성하는 방법

---
+++ Chapter 08. 클래스/8.1 클래스의 기본/8.1.1 클래스 선언/main.py
@@ -0,0 +1,17 @@
+class Student:
+    pass
+
+student = Student()
+
+# 학생 리스트 선언
+students = [
+    Student(),
+    Student(),
+    Student(),
+    Student(),
+    Student(),
+    Student(),
+    Student(),
+    Student()
+]
+print(students)
